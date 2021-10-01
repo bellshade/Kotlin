babe9404f4b1b9e6c1d70d48dbbcb3646de023f4
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-10-01 18:48:04 +0700

    add: program "Hello, Kotlin!"

---
+++ basic/main/00-hello-kotlin/HelloKotlin.kt
@@ -0,0 +1,8 @@
+// Ini adalah kode dasar menggunakan "Hello World" menggunakan kotlin!
+
+fun main() {
+
+  // Di kotlin, kita tidak wajib menuliskan ';' (titik koma)  di ujung kode
+  // Hal ini karena kotlin telah mengenali ujung dari kode itu sendiri
+  print("Hello, Kotlin!")
+}
