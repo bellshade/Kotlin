1e31920eb66e417551381e184b63c11771d15080
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-10-03 10:49:53 +0700

    add: basic tipe data boolean

---
+++ basic/main/01-variable-dan-tipe-data/Boolean.kt
@@ -0,0 +1,12 @@
+fun main(){
+  /**
+   * Tipe data boolean dideklarasikan dengan keyword `Boolean`. 
+   * Valuenya hanya boleh `true` atau `false` 
+   */
+  val bool1 : Boolean = true
+  println(bool1)
+
+  // atau bisa hanya dengan
+  val bool2 = true
+  println(bool2)
+}
